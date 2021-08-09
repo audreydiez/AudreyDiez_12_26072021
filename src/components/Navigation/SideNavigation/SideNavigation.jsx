8481435321f80b14be8ec0759b5af413a1cddc20
Audrey Diez <audrey.diez@gmail.com>
2021-08-09 17:03:27 +0200

    WIP: side-nav and main nav

--- src/components/Navigation/SideNavigation/SideNavigation.jsx
+++ src/components/Navigation/SideNavigation/SideNavigation.jsx
@@ -7,10 +7,31 @@ import yogaIcon from './../../../assets/images/yoga.svg'
 import swimIcon from './../../../assets/images/swiming.svg'
 import bicycleIcon from './../../../assets/images/vtt.svg'
 import weightIcon from './../../../assets/images/weight.svg'
+import { Link } from 'react-router-dom'
 
 class SideNavigation extends Component {
     render() {
-        return <div className="side-bar">side</div>
+        return (
+            <div className="side-bar">
+                <ul className="side-bar__icons">
+                    <li>
+                        <img src={yogaIcon} alt="Page d'accueil" />
+                    </li>
+                    <li>
+                        <img src={yogaIcon} alt="Page d'accueil" />
+                    </li>
+                    <li>
+                        <img src={yogaIcon} alt="Page d'accueil" />
+                    </li>
+                    <li>
+                        <img src={yogaIcon} alt="Page d'accueil" />
+                    </li>
+                </ul>
+                <Link to="/dashboard" className="side-bar__copyright">
+                    Copiryght, SportSee 2020
+                </Link>
+            </div>
+        )
     }
 }
 
