26e6228524065702403ea29413c0e17cf9ae15d7
Audrey Diez <audrey.diez@gmail.com>
2021-08-09 23:01:08 +0200

    feat: add burger menu

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom'
 class Navigation extends Component {
     getNavLinks = () => {
         return this.props.data.map((link, index) => (
-            <li key={index}>
+            <li key={index} className="main-nav__links">
                 <Link to="/dashboard" title={link}>
                     {link}
                 </Link>
@@ -15,16 +15,25 @@ class Navigation extends Component {
         ))
     }
 
+    handleClickBurger = () => {
+        console.log('clic')
+    }
+
     render() {
         return (
             <nav className="navigation">
                 <ul className="main-nav">
-                    <li>
+                    <li className="main-nav__links">
                         <Link to="/" className="main-nav--list--logo">
                             <img src={logo} alt="SportSee" />
                         </Link>
                     </li>
                     {this.getNavLinks()}
+                    <li className="main-nav__burger" onClick={this.handleClickBurger}>
+                        <span className="line" />
+                        <span className="line" />
+                        <span className="line" />
+                    </li>
                 </ul>
             </nav>
         )
